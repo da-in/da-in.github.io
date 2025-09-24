a496fc9181aede20971751c1ae4088b42d08e251
da-in <talown@naver.com>
2025-09-24 18:55:13 +0900

    :sparkles: Next.js 설정 업데이트 및 이미지 추가
    - `next.config.ts` 파일에 출력 형식 및 이미지 최적화 설정 추가.
    - `package.json`에 이미지 동기화 스크립트 추가.
    - 여러 개의 이미지 파일 및 파비콘 관련 파일을 새로 추가하여 프로젝트의 자산을 보강함.

--- next-site/next.config.ts
+++ next-site/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
